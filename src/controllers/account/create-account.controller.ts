4813d3ba74994c03fe33d502f812946e2907b994
root <root@MambooLab>
2024-11-08 11:48:20 +0100

    feat: update type

--- src/controllers/account/create-account.controller.ts
+++ src/controllers/account/create-account.controller.ts
@@ -1,14 +1,22 @@
 import { ConflictException, Body, Controller, Post } from '@nestjs/common'
-import { User } from '@prisma/client'
 import { PrismaService } from 'src/prisma/prisma.services'
 import { hash } from 'bcryptjs'
+import { z } from 'zod'
+
+const createAccountBodySchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  password: z.string(),
+})
+
+type CreateAccountBodySchema = z.infer<typeof createAccountBodySchema>
 
 @Controller('/accounts')
 export class CreateAccountController {
   constructor(private readonly prismaService: PrismaService) {}
 
   @Post()
-  async handle(@Body() body: User) {
+  async handle(@Body() body: CreateAccountBodySchema) {
     const { name, email, password } = body
 
     const userWithSomeEmail = await this.prismaService.user.findUnique({
